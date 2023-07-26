de57226d083d1dc4a85a45286e8653a81ac3b3e5
Silambarasan <msilambarasan98@gmail.com>
2023-07-26 17:37:54 +0530

    first commit

---
+++ Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assisted_Singly_Linked_Lists
+{
+    public class Node
+    {
+        private string data;
+        private Node next = null;
+        public string Data
+        {
+            get { return this.data; }
+            set { this.data = value; }
+        }
+
+        public Node Next
+        {
+            get { return this.next; }
+            set { this.next = value; }
+        }
+    }
+}
